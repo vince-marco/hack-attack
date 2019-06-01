66bb8a9809df13ae0b47fb99c30699f858a66d3b
Vince Marco <vince.marco@gmail.com>
2019-06-01 00:02:09 -0700

    VirtualSystem terminal update.
    Console updated by system. cmd terminal routing working.

--- unity/hack-attack/Assets/Virtual/Scripts/VirtualSystem.cs
+++ unity/hack-attack/Assets/Virtual/Scripts/VirtualSystem.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VirtualSystem : MonoBehaviour {
 
+    private float timeLeft = 30.0f;
+    [SerializeField] Text textview;
     [TextArea(2, 5)][SerializeField] string consoleGreeting;
     [TextArea(2, 5)][SerializeField] string term1Greeting;
     [TextArea(2, 5)][SerializeField] string term2Greeting;
@@ -17,6 +20,12 @@ public class VirtualSystem : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+      timeLeft -= Time.deltaTime;
+      if (timeLeft < 0) {
+        //Logger.info("VirtualSystem...running.");
+        textview.text += "VirtualSystem...running.\n\n";
+        timeLeft = 30.0f;
+      }
     }
     void FixedUpdate()
     {
---
+++ unity/hack-attack/Assets/Virtual/Scripts/VirtualTerm.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VirtualTerm : MonoBehaviour
+{
+    [SerializeField] Text term1;
+    [SerializeField] Text term2;
+    [SerializeField] Text term3;
+    [SerializeField] Text console;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var input = gameObject.GetComponent<InputField>();
+        input.onEndEdit.AddListener(SubmitCmd);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SubmitCmd(string cmd) 
+    {
+        var input = gameObject.GetComponent<InputField>();
+        if (input.text.Contains(" -1")) 
+        {
+            term1.text += "\n> ";
+            term1.text += input.text;
+            term1.text += "...executing\n";
+        }
+        else if (input.text.Contains(" -2")) 
+        {
+            term2.text += "\n> ";
+            term2.text += input.text;
+            term2.text += "...executing\n";
+        }
+        else if (input.text.Contains(" -3")) 
+        {
+            term3.text += "\n> ";
+            term3.text += input.text;
+            term3.text += "...executing\n";
+        }
+        else
+        {
+            console.text += "\n> ";
+            console.text += input.text;
+            console.text += "...executing\n";
+        }
+        input.text = "";
+    }
+}
